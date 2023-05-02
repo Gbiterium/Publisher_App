113a5aa37b405adbce354dc2e995a803d241d43c
Sami56 <adetona.samuel@e4email.net>
2023-05-02 14:30:42 +0100

    remove middleware

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,8 +1,8 @@
 import Toast from '~/helpers/Toast'
 
 export default function ({ $cookies, redirect, $route }) {
-    if ($cookies.get('slate-token') === undefined) {
-        Toast(' ', 'You must be logged in to access this page', 'warning')
-      redirect('https://www.slate.ng/auth/login?publisher=true')
-    }
+    // if ($cookies.get('slate-token') === undefined) {
+    //     Toast(' ', 'You must be logged in to access this page', 'warning')
+    //   redirect('https://slate-staging-bnt8w.ondigitalocean.app/auth/login?publisher=true')
+    // }
   }
